242233dacdedd59b30ddb7f57eed48e5f46b30c0
Ambar <ambar.aguilar.c@gmail.com>
2024-04-04 22:50:26 -0300

    WeatherAPP React

--- src/WeatherPage.jsx
+++ src/WeatherPage.jsx
@@ -1,135 +1,115 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
-const container = document.getElementById("contenedor");
-const search = document.querySelector("#button-search");
-const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weather-details');
-const errorNotFound = document.querySelector('.not-found');
+const SearchForm = ({ onSearch }) => {
+  const [city, setCity] = useState('');
 
-export const WeatherPage = () => {
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
 
-
-search.addEventListener('click', () => {
-
-    const APIKey = '7abd4d15956620992b29321544dfdea5';
-    const city = document.querySelector('.search-box input').value;
-
-    // const [apiUrl, setApiUrl] = useState(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`);
-
-    // useEffect(() => {
-    //   first
-    
-    //   return () => {
-    //     second
-    //   }
-    // }, [third])
-   
-
-    if( city === '')
-        return;
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-        .then( response => response.json() )
-        .then( json => {
-
-            if( json.cod === 404 ){
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                errorNotFound.style.display = 'block';
-                errorNotFound.classNameList.add('fadeIn');
-                return;
-            }
-
-            errorNotFound.style.display = 'none';
-            errorNotFound.classNameList.remove('fadeIn');
-
-            const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('.weather-details .wind span');
-
-            switch ( json.weather[0].main ) {
-                case 'Clear':
-                    image.src = 'images/clear.png';
-                    break;
-
-                case 'Rain':
-                    image.src = 'images/rain.png';
-                    break;
-
-                case 'Snow':
-                    image.src = 'images/snow.png';
-                    break;
-                    
-                case 'Clouds':
-                    image.src = 'images/cloud.png';
-                    break;     
-
-                case 'Haze':
-                    image.src = 'images/mist.png';
-                    break;  
-
-                default:
-                    image.src = '';
-            }
-
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classNameList.add('fadeIn');
-            weatherDetails.classNameList.add('fadeIn');
-            container.style.height = '590px';
-
-                
-        });
-        
-
-});
-
+  const handleSearch = () => {
+    onSearch(city);
+  };
 
   return (
-    <>
-        <div className="container" id="contenedor">
-          <div className="search-box">
-            <i className="fa-solid fa-location-dot"></i>
-            <input type="text" placeholder="Enter your location"/>
-            <button id="button-search" className="fa-solid fa-magnifying-glass"></button>
-          </div>
+    <div className="search-box">
+      <input type="text" placeholder="Ingrese una ciudad" value={city} onChange={handleInputChange} />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
 
-        <div className="not-found">
-          <img src="images/404-notfound.png"/>
-          <p>Location not found :/</p>
-        </div>
+const WeatherDisplay = ({ weatherData, error }) => {
 
-        <div className="weather-box">
-          <img src=""/>
-          <p className="temperature"></p>
-          <p className="description"></p>
-        </div>
+    console.log({weatherData});
 
-        <div className="weather-details">
-          <div className="humidity">
-            <i className="fa-solid fa-water"></i>
-            <div className="text">
-              <span></span>
-              <p>Humidity</p>
-            </div>
-          </div>
-          <div className="wind">
-            <i className="fa-solid fa-water"></i>
-            <div className="text">
-              <span></span>
-              <p>Wind speed</p>
-              </div>
-            </div>
-          </div>
-        </div>
-    </>
-  )
-}
+  if (error) {
+    return <div className="not-found">
+        <img src={"/images/404-notfound.png"}  />
+        <p>City not found</p>
+    </div>;
+    
+  }
+
+  if (!weatherData) {
+    return null;
+  }
+
+  const { main, weather, wind } = weatherData;
+  const { temp, humidity } = main;
+  const { description } = weather[0];
+
+  let weatherImageSrc = '';
+  switch (weather[0].main) {
+    case 'Clear':
+      weatherImageSrc = 'images/clear.png';
+      break;
+    case 'Rain':
+      weatherImageSrc = 'images/rain.png';
+      break;
+    case 'Snow':
+      weatherImageSrc = 'images/snow.png';
+      break;
+    case 'Clouds':
+      weatherImageSrc = 'images/cloud.png';
+       
+      break;
+    case 'Haze':
+      weatherImageSrc = 'images/mist.png';
+      break;
+    default:
+      weatherImageSrc = '';
+  }
+
+  return (
+    <div className="weather-box">
+      <img src={weatherImageSrc} alt="Weather" />
+      <div className="temperature">{parseInt(temp)}<span>°C</span></div>
+      <div className="description">{description}</div>
+      <div className="weather-details">
+        <div className="humidity">Humidity: <span>{humidity}%</span></div>
+        <div className="wind">Wind: <span>{parseInt(wind.speed)} Km/h</span></div>
+      </div>
+    </div>
+  );
+};
+
+export const WeatherPage = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const APIKey = '7abd4d15956620992b29321544dfdea5';
+
+  const fetchWeatherData = (city) => {
+    if (!city) return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('City not found');
+        }
+        return response.json();
+      })
+      .then(json => {
+        setWeatherData(json);
+        setError(null);
+      })
+      .catch(error => {
+        setWeatherData(null);
+        setError(error.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchWeatherData(''); // Initial default city (you can change this)
+  }, [setWeatherData]);
+
+  return (
+    <div className="container">
+      <SearchForm onSearch={fetchWeatherData} />
+      <WeatherDisplay weatherData={weatherData} error={error} />
+    </div>
+  );
+};
+
+
